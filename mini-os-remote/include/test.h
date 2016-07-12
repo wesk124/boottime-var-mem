dd1bbeee9e4c2fa95c83486be40f90daeff1a0a5
Sheng Wei <shengw12@vt.edu>
2016-07-12 13:02:13 -0400

    remove submodule

---
+++ mini-os-remote/include/test.h
@@ -0,0 +1,5 @@
+// This file is for experimental global variables.
+
+unsigned long long ull1, ull2;
+
+
---
+++ mini-os-remote/test.h
@@ -0,0 +1,12 @@
+#define __TEST_H__
+#ifndef __TEST_H__
+
+#include <time.h>
+
+extern struct timeval t1, t2;
+extern uint64_t ut1, ut2;
+
+
+
+
+#endif
