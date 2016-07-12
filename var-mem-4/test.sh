5921065c9ece6d23b33e273158a09b58620560cf
Sheng Wei <shengw12@vt.edu>
2016-07-12 10:16:22 -0400

    clear useless files

--- var-mem-4/test.sh
+++ var-mem-4/test.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/sh
 
 # Declare variables
 
@@ -11,41 +11,43 @@ VAR_DESTRO="destroy"
 VAR_MEMORY="30"
 VAR_VCPU="1"
 
-#if [ ! $# == "2" ]; then
-#	echo "Usage: $0 <max num. of unikernels> <time between xl creates (s)>"
-#	exit
-#fi
+if [ ! $# == "2" ]; then
+	echo "Usage: $0 <max num. of unikernels> <time between xl creates (s)>"
+	exit
+fi
 
-#if [ ! `whoami` == "root" ]; then
-#	echo "Please run as root"
-#	exit
-#fi
-
-# generate the target domain config file
-target=/etc/xen/target_app.cfg
-echo -n "" > $target
-echo "kernel=\"$VAR_KERNEL\"" >> $target
-echo "memory=$VAR_MEMORY" >> $target
-echo "vcpus=$VAR_VCPU" >> $target
-echo "name=\"test_app$i\"" >> $target
-echo "on_crash=\"$VAR_DESTRO\"" >> $target
-
-
+if [ ! `whoami` == "root" ]; then
+	echo "Please run as root"
+	exit
+fi
 
 # generating the configure files
 for i in `seq 0 $COUNT_OF_CONFIGS`; do
-	temp=/etc/xen/test_app$i.cfg
-	echo -n "" > $temp
-	echo "kernel=\"$VAR_KERNEL\"" >> $temp
-	echo "memory=$VAR_MEMORY" >> $temp
-	echo "vcpus=$VAR_VCPU" >> $temp
-	echo "name=\"test_app$i\"" >> $temp
-	echo "on_crash=\"$VAR_DESTRO\"" >> $temp
+	target=/etc/xen/test_app$i.cfg
+	echo -n "" > $target
+	echo "kernel=\"$VAR_KERNEL\"" >> $target
+	echo "memory=$VAR_MEMORY" >> $target
+	echo "vcpus=$VAR_VCPU" >> $target
+	echo "name=\"test_app$i\"" >> $target
+	echo "on_crash=\"$VAR_DESTRO\"" >> $target
 done
 
 # Reset result file
 echo -n "" > data.txt
 
-echo $(xl create /etc/xen/target_app.cfg -q -c | grep "time is:" | cut -d " " -f 4 )>> data.txt &
+# Main loop of the experiment
+for i in `seq 0 $COUNT_OF_CONFIGS`; do
+	# Write in the target appiliance boot time
+	VAR_RESULT=$(chrono xl create /etc/xen/test_app$i.cfg)
+	echo $i $VAR_RESULT >> data.txt 
+	echo "done $i/$COUNT_OF_CONFIGS"
+	sleep $2
+done
 
+# Destroy all appliances and remove all configure files
 
+for i in `seq 0 $COUNT_OF_CONFIGS`; do 
+	xl destroy test_app$i
+	rm /etc/xen/test_app$i.cfg
+	echo "destroyed $i/$COUNT_OF_CONFIGS"
+done
