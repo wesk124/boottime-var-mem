ed60d6bd86477a510eea728a6e7bb0811c8af75a
Sheng Wei <shengw12@vt.edu>
2016-07-25 14:48:10 -0400

    update exp6

--- exp6/test.sh
+++ exp6/test.sh
@@ -1,26 +1,21 @@
 #!/bin/sh
 
-if [ $# -ne 1 ]; then 
+if [ $# -ne 2 ]; then 
 
-    echo "Usage:  $0 <max num of microsec of sleep()>"
+    echo "Usage:  $0 <max num of microsec of sleep()> <num of iterations of xentop>"
     exit
 fi  
 
 # Initializations of data files
-echo -n "" > cpu-log.dat
-echo -n "" > avg-stddiv.dat
+
+echo -n "" > avg-stddev.dat
 
 for i in `seq 1 $1 `;do 
     # Prepare the to items in the xenstore we will access
     xenstore-write /test/read_access $i
-    #xenstore-write /test/write_access ""
 
     # Give the permission to our VM which will be named "Mini-OS"
-    # read for the first, and read + write for the second item
     xenstore-chmod /test/read_access rMini-OS
-    #xenstore-chmod /test/write_access bMini-OS
-
-
 
     # Launch unikernel
     make run
@@ -33,14 +28,16 @@ for i in `seq 1 $1 `;do
 
     xenstore-read /test/read_access
 
-    #echo "test/write_access read from dom0:"
-
-    #xenstore-read /test/write_access
-
     sleep 3
 
-    echo "microsec of usleep(): "$i >> cpu-log.dat
-    xentop -b -i 11 | grep Mini | awk '{if(NR > 1) {print $4}}' >> cpu-log.dat
+    echo -n "" > cpu-log.dat
+    #echo "$i" >> cpu-log.dat
+    xentop -b -i $(($2+1)) | grep Mini | awk '{if(NR > 1) {print $4}}' >> cpu-log.dat 
+    echo "microsec: " $i >> avg-stddev.dat
+    echo "average: " >> avg-stddev.dat
+    awk '{if(NR >= 1) {sum += $1}} END {print sum/ NR }' cpu-log.dat >> avg-stddev.dat
+    echo "standard deviation: " >> avg-stddev.dat
+    awk '{if(NR >= 1) {sum += $1; sumsq +=$1*$1}} END {print sqrt(sumsq/NR - (sum/NR)**2)}' cpu-log.dat >> avg-stddev.dat
 
 
     # Cleanup the xenstore
@@ -54,3 +51,5 @@ for i in `seq 1 $1 `;do
     echo "done" $i/$1
 done
 
+
+   
