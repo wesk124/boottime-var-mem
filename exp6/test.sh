f4ea59f03d9aa0be8b4531a88138cdf3623da6dc
Pierre Olivier <polivier@vt.edu>
2016-07-26 20:51:19 -0400

    Merged my modifs from ukboot

--- exp6/test.sh
+++ exp6/test.sh
@@ -1,16 +1,16 @@
 #!/bin/sh
 
-if [ $# -ne 2 ]; then 
+if [ $# -ne 4 ]; then 
 
-    echo "Usage:  $0 <max num of microsec of sleep()> <num of iterations of xentop>"
-    exit
+   echo "Usage:  $0 <start_ns> <step_ns> <stop_ns> <xentop_time_s>"
+   echo "  Iterates from start_ns to stop_ns with a step of step_ns"
+   exit
 fi  
 
 # Initializations of data files
-
 echo -n "" > avg-stddev.dat
 
-for i in `seq 1 $1 `;do 
+for i in `seq $1 $2 $3`;do 
     # Prepare the to items in the xenstore we will access
     xenstore-write /test/read_access $i
 
@@ -18,33 +18,43 @@ for i in `seq 1 $1 `;do
     xenstore-chmod /test/read_access rMini-OS
 
     # Launch unikernel
-    make run
+    xl create domain_config.xl -q
 
     # wait 3 secs for unikernel bootting
     sleep 3
 
     # echo passing message
-    echo "test/read_access read from dom0:"
+    # echo "test/read_access read from dom0:"
 
-    xenstore-read /test/read_access
+    # xenstore-read /test/read_access
 
-    sleep 3
+    # sleep 3
 
     echo -n "" > cpu-log.dat
-    xentop -b -i $(($2+1)) | grep Mini | awk '{if(NR > 1) {print $4}}' >> cpu-log.dat 
+    xentop -b -d1 -i $(($4+1)) | grep Mini | awk '{if(NR > 1) {print $4}}' >> cpu-log.dat 
     awk -v microsec=$i '{if(NR >= 1) {sum += $1; sumsq +=$1*$1}} END {print microsec "," sum/NR "," sqrt(sumsq/NR - (sum/NR)**2)}' cpu-log.dat >> avg-stddev.dat
-
+    
+    # clean temp file
+    rm cpu-log.dat
 
     # Cleanup the xenstore
     xenstore-rm /test
 
-    echo "xenstore path test/ has been cleaned."
+    # echo "xenstore path test/ has been cleaned."
 
     xl shutdown -a
 
-    echo "shutdown guest domain."
+    # echo "shutdown guest domain."
     echo "done" $i/$1
 done
+
+# Plot if gnuplot is installed
+# gp=`which gnuplot`
+#
+# if [ "$gp" == "" ]; then
+#     echo "Gnuplot not detected, no plot."
+#     exit 0
+# fi
 
 
    
